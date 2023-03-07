e86c1b9ba59e6f4ef5e30633546c171732bacbbd
Khalil Ibrahim <100088472+khalil979@users.noreply.github.com>
2023-03-07 15:05:37 +0300

    updated

--- app.py
+++ app.py
@@ -1,58 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
+from flask import Flask, render_template, session, redirect
+from functools import wraps
+import pymongo
 
 app = Flask(__name__)
+# app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
+app.secret_key = "HEEELLLLOOO"
 
-# Set up database connection
-client = MongoClient(
-    'mongodb://localhost:27017')
-db = client['mydatabase']
+# Database
+client = pymongo.MongoClient('localhost', 27017)
+db = client.user_login_system
 
-# Define sign-up page
+# Decorators
+def login_required(f):
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return f(*args, **kwargs)
+    else:
+      return redirect('/')
+  
+  return wrap
 
+# Routes
+from user import routes
 
-@app.route("/signup", methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        # Get form data
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
+@app.route('/')
+def home():
+  return render_template('home.html')
 
-        # Check if user already exists
-        if db.users.find_one({'email': email}):
-            return 'User already exists'
-
-        # Insert user data into database
-        db.users.insert_one(
-            {'name': name, 'email': email, 'password': password})
-
-        return 'Sign up successful'
-
-    # Render sign-up page
-    return render_template('signup.html')
-
-
-@app.route("/dashboard")
+@app.route('/dashboard/')
+@login_required
 def dashboard():
-    return 'Welcome to Dashboard'
+  return render_template('dashboard.html')
 
 
-@app.route("/", methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = db.users.find_one({'email': email})
-        if user:
-            if user['password'] == password:
-                return redirect(url_for('dashboard'))
-            else:
-                error = 'Invalid Credentials. Please try again.'
-        else:
-            error = 'User does not exist. Please sign up first.'
-    return render_template('login.html', error=error)
-
-
-app.run(host="localhost", port=8000)
+if __name__ == "__main__":
+  app.run(port=8500)
